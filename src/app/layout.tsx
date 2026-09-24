17cb8f714d25406f0ea7cb46bc2acd2469908d2a
Mrinal <mrinaal2006@github.com>
2026-09-25 04:20:47 +0530

    Hide dev tools and disable right click

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,20 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              document.addEventListener('contextmenu', event => event.preventDefault());
+              document.onkeydown = function(e) {
+                if(e.keyCode == 123) { return false; } // F12
+                if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) { return false; }
+                if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) { return false; }
+                if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) { return false; }
+                if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) { return false; } // Ctrl+U
+              }
+            `
+          }}
+        />
         <Providers>
           <header className="header">
             <div className="container header-content">
